08f34b0f37ad74845366bb0b2847ef4ee2e4ab6c
Arturo Aliaga <jaav2801@gmail.com>
2024-12-02 17:31:43 -0400

    Bitacora

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backendfitmrp-production.up.railway.app/api',  // Cambia esto a la URL de tu backend
+  baseURL: 'https://backendfitmrp-production.up.railway.app/api', // Cambia según la URL de tu backend
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // Obtén el token desde localStorage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
